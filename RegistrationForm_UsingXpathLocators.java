b3c8b43769d46490033eebff404072270fc010c7
VidyaJayaraman <112783853+VidyaJayaraman@users.noreply.github.com>
2022-09-12 14:52:20 +0200

    Add files via upload

---
+++ RegistrationForm_UsingXpathLocators.java
@@ -0,0 +1,43 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class RegistrationForm_UsingXpathLocators {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("DemoSalesManager");
+		
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+		
+		driver.findElement(By.xpath("//input[contains(@class,'decorative')]")).click();
+		
+		driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+		
+		driver.findElement(By.xpath("//a[text() = 'Leads']")).click();
+		
+		driver.findElement(By.xpath("//a[text() = 'Create Lead']")).click();
+		
+		driver.getTitle();
+		
+		driver.findElement(By.xpath("(//input[@name='companyName'])[2]")).sendKeys("TestLeaf");
+		
+		driver.findElement(By.xpath("//input[@id='createLeadForm_firstName']")).sendKeys("Vidya");
+		
+		driver.findElement(By.xpath("//input[@id='createLeadForm_lastName']")).sendKeys("Vidya");
+		
+		driver.findElement(By.xpath("//input[@name='submitButton']")).click();
+		
+	}
+
+}
